71f14ecd54df8ac029c0b0bc0a27f04d4f5ccaf7
Christoffer Hennie <christoffer@hennie.dev>
2025-04-19 00:39:06 +0200

    Refactor layout.tsx for improved consistency in import statements and formatting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,30 @@
-import {Inter} from 'next/font/google';
-import './globals.css';
+import {Inter} from "next/font/google";
+import "./globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
     ? `https://${process.env.VERCEL_URL}`
-    : 'http://localhost:3000';
+    : "http://localhost:3000";
 
 export const metadata = {
     metadataBase: new URL(defaultUrl),
-    title: 'Navet - Project Unagi',
-    description: 'Bindeledd mellom studenter og næringslivet for studentene på institutt for informatikk ved UiO.',
+    title: "Navet - Project Unagi",
+    description:
+        "Bindeledd mellom studenter og næringslivet for studentene på institutt for informatikk ved UiO.",
 };
 
 const interSans = Inter({
-    display: 'swap',
-    subsets: ['latin'],
+    display: "swap",
+    subsets: ["latin"],
 });
 
-export default function RootLayout({children,}: Readonly<{
+export default function RootLayout({
+    children,
+}: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
         <html lang="no" className={interSans.className} suppressHydrationWarning>
-        <body className="bg-background text-foreground">
-        {children}
-        </body>
+            <body>{children}</body>
         </html>
     );
 }
