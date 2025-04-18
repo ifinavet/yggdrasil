b2fbf729e4400070d79455f5ec2606931572efc2
Christoffer Hennie <christoffer.hennie@outlook.com>
2025-04-19 00:38:52 +0200

    Fix import order in client.ts for improved clarity and consistency

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,5 +1,5 @@
+import type {Database} from "@/database.types";
 import {createBrowserClient} from "@supabase/ssr";
-import {Database} from "@/database.types";
 
 export const createClient = () =>
     createBrowserClient<Database>(
