0a9954af7d3d876f3bba541e91d0df607337fe8b
Christoffer Hennie <christoffer.hennie@outlook.com>
2025-04-19 00:38:11 +0200

    Refactor authentication actions for improved readability and consistency; streamline error handling and formatting

--- app/(frontend)/(auth-pages)/sign-in/page.tsx
+++ app/(frontend)/(auth-pages)/sign-in/page.tsx
@@ -1,5 +1,5 @@
 import {signInAction} from "@/app/(frontend)/actions";
-import {FormMessage, Message} from "@/components/form-message";
+import {FormMessage, type Message} from "@/components/form-message";
 import {SubmitButton} from "@/components/submit-button";
 import {Input} from "@/components/ui/input";
 import {Label} from "@/components/ui/label";
@@ -18,26 +18,18 @@ export default async function Login(props: { searchParams: Promise<Message> }) {
             </p>
             <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
                 <Label htmlFor="email">Email</Label>
-                <Input name="email" placeholder="you@example.com" required/>
+                <Input name="email" placeholder="you@example.com" required />
                 <div className="flex justify-between items-center">
                     <Label htmlFor="password">Password</Label>
-                    <Link
-                        className="text-xs text-foreground underline"
-                        href="/forgot-password"
-                    >
+                    <Link className="text-xs text-foreground underline" href="/forgot-password">
                         Forgot Password?
                     </Link>
                 </div>
-                <Input
-                    type="password"
-                    name="password"
-                    placeholder="Your password"
-                    required
-                />
+                <Input type="password" name="password" placeholder="Your password" required />
                 <SubmitButton pendingText="Signing In..." formAction={signInAction}>
                     Sign in
                 </SubmitButton>
-                <FormMessage message={searchParams}/>
+                <FormMessage message={searchParams} />
             </div>
         </form>
     );
