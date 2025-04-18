3b7d170011490db1e2fefd2aaa75c24fbf5f5af0
Christoffer Hennie <christoffer@hennie.dev>
2025-04-19 00:39:43 +0200

    Refactor header and layout components for improved structure and readability; add ThemeSwitcher to header

--- components/dashboard/header.tsx
+++ components/dashboard/header.tsx
@@ -1,17 +1,18 @@
-'use client'
+"use client";
 
-import {SidebarTrigger} from "@/components/ui/sidebar";
-import {Separator} from "@/components/ui/separator";
 import {useTitle} from "@/app/contexts/TitleContext";
+import {ThemeSwitcher} from "@/components/theme-switcher";
+import {Separator} from "@/components/ui/separator";
+import {SidebarTrigger} from "@/components/ui/sidebar";
 
 export default function Header() {
-    const {title} = useTitle()
+    const { title } = useTitle();
 
     return (
-        <header
-            className="group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-12 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear">
+        <header className="group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-12 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear">
             <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
-                <SidebarTrigger className="-ml-1"/>
+                <SidebarTrigger className="-ml-1" />
+                <ThemeSwitcher />
                 <Separator
                     orientation="vertical"
                     className="mx-2 data-[orientation=vertical]:h-4"
@@ -19,5 +20,5 @@ export default function Header() {
                 <h1 className="text-base font-medium">{title}</h1>
             </div>
         </header>
-    )
+    );
 }
