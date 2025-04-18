f3c9892feeed4f6982c88cb79d498623ac3db6ce
Christoffer Hennie <christoffer@hennie.dev>
2025-04-19 00:38:11 +0200

    Refactor authentication actions for improved readability and consistency; streamline error handling and formatting

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -1,9 +1,9 @@
-import Hero from "@/components/hero";
+import Header from "@/components/(frontend)/layout-components/header";
 
 export default async function Home() {
     return (
         <>
-            <Hero/>
+            <Header />
             <main className="flex-1 flex flex-col gap-6 px-4">
                 <h1>Welcome to Navet</h1>
             </main>
