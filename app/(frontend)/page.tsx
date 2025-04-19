a1366345d22c7393b6b18624450bc8962bb8f7c6
Christoffer Hennie <christoffer.hennie@outlook.com>
2025-04-19 23:00:31 +0200

    Refactor password reset form; simplify import paths and enhance code formatting

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -1,11 +1,11 @@
-import Header from "@/components/(frontend)/layout-components/header";
+import Link from "next/link";
 
 export default async function Home() {
     return (
         <>
-            <Header />
-            <main className="flex-1 flex flex-col gap-6 px-4">
+            <main className='flex-1 flex flex-col gap-6 px-4'>
                 <h1>Welcome to Navet</h1>
+                <Link href='/sign-in'>Logg inn</Link>
             </main>
         </>
     );
