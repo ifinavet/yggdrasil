382d0999e8fec7d0b9a4f1423f6e94637e37c751
Christoffer Hennie <christoffer@hennie.dev>
2025-06-14 15:04:15 +0200

    Refactor and re-formatting

--- src/app/(bifrost)/bifrost/events/create-event/actions.ts
+++ src/app/(bifrost)/bifrost/events/create-event/actions.ts
@@ -3,77 +3,84 @@ import { createClient } from "@/utils/supabase/client";
 import type { z } from "zod";
 
 export async function getCompanies() {
-    const supabase = createClient();
-    const { data: companies } = await supabase.from("company").select("id, name");
-    return companies ?? [];
+  const supabase = createClient();
+  const { data: companies } = await supabase.from("company").select("id, name");
+  return companies ?? [];
 }
 
 export async function getInternalMembers() {
-    const supabase = createClient();
-    const { data: internal_members } = await supabase
-        .from("internal_member")
-        .select("id, user (id, firstname, lastname)")
-        .overrideTypes<
-            {
-                id: string;
-                user: { id: string; firstname: string; lastname: string };
-            }[]
-        >();
+  const supabase = createClient();
+  const { data: internal_members, error: internal_member_error } =
+    await supabase
+      .from("internal_member")
+      .select("id, user (id, firstname, lastname)")
+      .overrideTypes<
+        {
+          id: string;
+          user: { id: string; firstname: string; lastname: string };
+        }[]
+      >();
 
-    const mappedMembers = internal_members?.map((member) => ({
-        id: member.id,
-        firstname: member.user.firstname,
-        lastname: member.user.lastname,
-        fullname: `${member.user.firstname} ${member.user.lastname}`,
-    }));
+  if (internal_member_error) {
+    console.error(internal_member_error);
+  }
 
-    return mappedMembers ?? [];
+  const mappedMembers = internal_members?.map((member) => ({
+    id: member.id,
+    firstname: member.user.firstname,
+    lastname: member.user.lastname,
+    fullname: `${member.user.firstname} ${member.user.lastname}`,
+  }));
+
+  return mappedMembers ?? [];
 }
 
-export async function submitEvent(formValues: z.infer<typeof formSchema>): Promise<boolean> {
-    const supabase = createClient();
-    const { data: event_res, error: event_err_res } = await supabase
-        .from("event")
-        .insert({
-            title: formValues.title,
-            description: formValues.description,
-            teaser: formValues.teaser,
-            event_date: formValues.eventDate.toISOString(),
-            registration_date: formValues.registrationDate.toISOString(),
-            location: formValues.location,
-            food: formValues.food,
-            language: formValues.language,
-            age_restriction: formValues.ageRestrictions,
-            participation_limit: formValues.participantsLimit,
-            event_type: formValues.eventType,
-            hosting_company: formValues.hostingCompany.id,
-            external_url: formValues.externalUrl,
-        })
-        .select("id")
-        .single();
+export async function submitEvent(
+  formValues: z.infer<typeof formSchema>,
+): Promise<boolean> {
+  const supabase = createClient();
+  const { data: event_res, error: event_err_res } = await supabase
+    .from("event")
+    .insert({
+      title: formValues.title,
+      description: formValues.description,
+      teaser: formValues.teaser,
+      event_date: formValues.eventDate.toISOString(),
+      registration_date: formValues.registrationDate.toISOString(),
+      location: formValues.location,
+      food: formValues.food,
+      language: formValues.language,
+      age_restriction: formValues.ageRestrictions,
+      participation_limit: formValues.participantsLimit,
+      event_type: formValues.eventType,
+      hosting_company: formValues.hostingCompany.id,
+      external_url: formValues.externalUrl,
+    })
+    .select("id")
+    .single();
 
-    if (event_res === null) {
-        console.error("Error inserting event:", event_err_res);
-        throw event_err_res;
-    }
+  if (event_res === null) {
+    console.error("Error inserting event:", event_err_res);
+    throw event_err_res;
+  }
 
-    const { data: organizers_res, error: organizers_err_res } = await supabase
-        .from("event_organizers")
-        .insert(
-            formValues.organizers.map((organizer) => {
-                return {
-                    event: event_res.id,
-                    navet_member: organizer.id,
-                    type: organizer.role,
-                };
-            }),
-        )
-        .select();
+  const { data: organizers_res, error: organizers_err_res } = await supabase
+    .from("event_organizers")
+    .insert(
+      formValues.organizers.map((organizer) => {
+        return {
+          event: event_res.id,
+          navet_member: organizer.id,
+          type: organizer.role,
+        };
+      }),
+    )
+    .select();
 
-    if (organizers_res === null) {
-        console.error("Error inserting event organizers:", organizers_err_res);
-        throw organizers_err_res;
-    }
+  if (organizers_res === null) {
+    console.error("Error inserting event organizers:", organizers_err_res);
+    throw organizers_err_res;
+  }
 
-    return true;
+  return true;
 }
--- src/app/(bifrost)/bifrost/layout.tsx
+++ src/app/(bifrost)/bifrost/layout.tsx
@@ -7,68 +7,70 @@ import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
 export interface User {
-    id: string;
-    email: string;
-    firstname: string;
-    lastname: string;
-    name: string;
-    initials: string;
-    avatar_url: string;
+  id: string;
+  email: string;
+  firstname: string;
+  lastname: string;
+  name: string;
+  initials: string;
+  avatar_url: string;
 }
 
 export default async function Layout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    const supabase = await createClient();
+  const supabase = await createClient();
 
-    const {
-        data: { user },
-    } = await supabase.auth.getUser();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-    if (!user) {
-        console.warn("User not found");
-        return redirect("/sign-in");
-    }
+  if (!user) {
+    console.warn("User not found");
+    return redirect("/sign-in");
+  }
 
-    const { data: internal_member, error } = await supabase
-        .from("internal_member")
-        .select("id, profile_image_id, user (id, firstname, lastname)")
-        .eq("id", user?.id)
-        .single()
-        .overrideTypes<{
-            id: string;
-            profile_image_id: string;
-            user: { id: string; firstname: string; lastname: string };
-        }>();
+  const { data: internal_member, error: internal_member_error } = await supabase
+    .from("internal_member")
+    .select("id, profile_image_id, user (id, firstname, lastname)")
+    .eq("id", user?.id)
+    .single()
+    .overrideTypes<{
+      id: string;
+      profile_image_id: string;
+      user: { id: string; firstname: string; lastname: string };
+    }>();
 
-    if (internal_member == null && error != null) {
-        console.warn(`Error fetching internal member: ${error}`);
-        return redirect("/sign-in");
-    }
+  if (internal_member == null && internal_member_error != null) {
+    console.log("Error fetching internal member: ", internal_member_error);
+    return redirect("/sign-in");
+  }
 
-    const imageUrl = await getInternalMemberImage(internal_member?.profile_image_id ?? "");
+  const imageUrl = await getInternalMemberImage(
+    internal_member?.profile_image_id ?? "",
+  );
 
-    const currentUser: User = {
-        id: user.id,
-        email: user.email ?? "",
-        firstname: internal_member.user.firstname,
-        lastname: internal_member.user.lastname,
-        name: `${internal_member.user.firstname} ${internal_member?.user.lastname}`,
-        initials: `${internal_member.user.firstname?.[0] ?? ""}${internal_member.user.lastname?.[0]}`,
-        avatar_url: imageUrl ?? "",
-    };
+  const currentUser: User = {
+    id: user.id,
+    email: user.email ?? "",
+    firstname: internal_member.user.firstname,
+    lastname: internal_member.user.lastname,
+    name: `${internal_member.user.firstname} ${internal_member?.user.lastname}`,
+    initials: `${internal_member.user.firstname?.[0] ?? ""}${internal_member.user.lastname?.[0]}`,
+    avatar_url: imageUrl ?? "",
+  };
 
-    return (
-        <TitleProvider>
-            <SidebarProvider>
-                <NavSidebar user={currentUser} />
-                <SidebarInset>
-                    <Header />
-                    <main className='p-4'>{children}</main>
-                </SidebarInset>
-            </SidebarProvider>
-        </TitleProvider>
-    );
+  return (
+    <TitleProvider>
+      <SidebarProvider>
+        <NavSidebar user={currentUser} />
+        <SidebarInset>
+          <Header />
+          <main className="p-4">{children}</main>
+        </SidebarInset>
+      </SidebarProvider>
+    </TitleProvider>
+  );
 }
