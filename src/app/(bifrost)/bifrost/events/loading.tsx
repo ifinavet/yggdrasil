1938823e15f68e7319c7482d98dd214b4c8e4ac6
Christoffer Hennie <christoffer.hennie@outlook.com>
2025-06-18 23:09:45 +0200

    Add loading skeleton for events page and update Skeleton style

---
+++ src/app/(bifrost)/bifrost/events/loading.tsx
@@ -0,0 +1,90 @@
+import { Plus, Users } from "lucide-react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function EventsLoading() {
+  return (
+    <>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/bifrost">Hjem</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Arrangementer</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      <div className="flex justify-between">
+        <Skeleton className="h-9 w-48" />
+        <Button>
+          <Plus className="size-4" /> Lag et nytt arrangement
+        </Button>
+      </div>
+
+      <div className="grid gap-6">
+        <h2 className="scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight first:mt-0">
+          Publiserte arrangementer
+        </h2>
+        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {Array.from({ length: 6 }).map((_, index) => (
+            <EventCardSkeleton key={`visible-skeleton-${index}`} />
+          ))}
+        </div>
+        <h2 className="scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight first:mt-0">
+          Skjulte/Påbegynte arrangementer
+        </h2>
+        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {Array.from({ length: 3 }).map((_, index) => (
+            <EventCardSkeleton key={`hidden-skeleton-${index}`} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+function EventCardSkeleton() {
+  return (
+    <Card className="h-full">
+      <CardHeader>
+        <Skeleton className="h-6 w-3/4" />
+        <Skeleton className="h-4 w-1/2" />
+      </CardHeader>
+      <CardContent className="flex flex-col gap-4 flex-1">
+        <div className="flex flex-row gap-2 flex-wrap">
+          <Skeleton className="h-4 w-32" />
+        </div>
+        <div className="flex gap-2 flex-wrap">
+          <Skeleton className="h-6 w-24 rounded-full" />
+          <Skeleton className="h-6 w-20 rounded-full" />
+        </div>
+      </CardContent>
+      <CardFooter className="flex-col items-start gap-1.5 text-sm">
+        <div className="line-clamp-1 flex gap-2 font-medium">
+          <Users className="size-4" /> Ansvarlige
+        </div>
+        <div className="text-muted-foreground space-y-1">
+          <Skeleton className="h-4 w-40" />
+          <Skeleton className="h-4 w-36" />
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
