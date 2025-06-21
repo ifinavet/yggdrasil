eeed1454f59bc854422702914525a1f1300f83a7
Christoffer Hennie <christoffer@hennie.dev>
2025-06-21 17:25:08 +0200

    Moved .git to parent directory

--- src/shared/constants/index.ts
+++
@@ -1,8 +0,0 @@
-// Constants exports
-// Add your constant exports here as you create them
-// Example:
-// export { API_ENDPOINTS } from './api-endpoints';
-// export { DEFAULT_VALUES } from './default-values';
-// export { VALIDATION_RULES } from './validation-rules';
-
-// This file will be populated as constants are added to the project
