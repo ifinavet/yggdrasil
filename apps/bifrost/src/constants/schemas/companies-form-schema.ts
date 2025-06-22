3c3d8fd1df5ec16be985d631391e0ec20b394f2a
Christoffer Hennie <christoffer@hennie.dev>
2025-06-22 16:02:37 +0200

    Add custom validation messages to form schemas

--- apps/bifrost/src/constants/schemas/companies-form-schema.ts
+++ apps/bifrost/src/constants/schemas/companies-form-schema.ts
@@ -1,13 +1,13 @@
 import z from "zod/v4";
 
 export const formSchema = z.object({
-  company_name: z.string().min(2),
-  org_number: z.string().min(9),
-  description: z.string().min(10),
+  company_name: z.string().min(2, "Venligst oppgi et navn på bedriften"),
+  org_number: z.string().min(9, "Venligst oppgi et fult organisasjonsnummer"),
+  description: z.string().min(10, "Venligst oppgi en beskrivelse på bedriften, mist 10 tegn."),
   company_image: z.object({
     id: z.string(),
     name: z.string(),
-  }),
+  }, "Venligst velg et bilde"),
 });
 
 export type CompanyFormValues = z.infer<typeof formSchema>;
--- apps/bifrost/src/constants/schemas/resource-form-schema.ts
+++ apps/bifrost/src/constants/schemas/resource-form-schema.ts
@@ -1,10 +1,10 @@
 import z from "zod/v4";
 
 export const resourceSchema = z.object({
-	title: z.string().min(2).max(80),
-	content: z.string().min(1),
-	excerpt: z.string().min(10).max(200),
-	tag: z.optional(z.string()),
+  title: z.string().min(2, "Gi ressursen et bra navn").max(80, "Navnet er for langt, maks 80 tegn"),
+  content: z.string().min(1, "Skulle vi kanskje hatt noe innhold?"),
+  excerpt: z.string().min(10, "Lag en kort beskrivelse av ressursen").max(200, "Beskrivelsen kan kan være maks 200 tegn."),
+  tag: z.optional(z.string()),
 });
 
 export type ResourceFormValues = z.infer<typeof resourceSchema>;
