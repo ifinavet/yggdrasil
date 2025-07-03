0a7c6722a897434c8a4855194b6121dfe3264e23
Christoffer Hennie <christoffer@hennie.dev>
2025-07-03 10:26:09 +0200

    Add breadcrumbs and improve job listings grouping logic

--- apps/bifrost/src/app/job-listings/new-listing/page.tsx
+++ apps/bifrost/src/app/job-listings/new-listing/page.tsx
@@ -1,5 +1,32 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@workspace/ui/components/breadcrumb";
 import NewJobListingForm from "./new-job-listing-form";
 
 export default async function NewListingPage() {
-	return <NewJobListingForm />;
+  return (
+    <>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href='/'>Hjem</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink href='/job-listings'>Stillingsannonser</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbItem>Opprett ny stillingsannonse</BreadcrumbItem>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      <NewJobListingForm />
+    </>
+  );
 }
