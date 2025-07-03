0b1ec33b3d1c610437443941ce998ecfe39ea600
Christoffer Hennie <christoffer@hennie.dev>
2025-07-03 22:15:20 +0200

    Refactor profile page and add Points component
    - Move points logic to new Points component - Update Registrations component with cards and event
    info - Improve layout and styling of profile page

--- apps/midgard/src/app/profile/page.tsx
+++ apps/midgard/src/app/profile/page.tsx
@@ -1,8 +1,8 @@
 import { auth } from "@clerk/nextjs/server";
 import { Title } from "@/components/common/title";
+import Points from "@/components/profile/points";
 import Registrations from "@/components/profile/registrations";
 import UpdateProfileForm from "@/components/profile/update-profile-form";
-import { getPointsById, getRegistrationsById } from "@/lib/query/profile";
 
 export default async function ProfilePage() {
   const { userId, redirectToSignIn } = await auth();
@@ -11,26 +11,24 @@ export default async function ProfilePage() {
     return redirectToSignIn();
   }
 
-  const points = await getPointsById(userId);
-
   return (
-    <div className='mx-auto max-w-5xl md:w-3/5'>
+    <div className="mx-auto max-w-5xl px-8 md:w-6/7 xl:w-3/5">
       <Title>Profil</Title>
-      <div className='flex flex-col gap-6'>
+      <div className='grid gap-6 md:grid-cols-2'>
         <div>
-          <h2 className='scroll-m-20 border-b pb-2 font-semibold text-3xl tracking-tight first:mt-0'>
+          <h2 className='scroll-m-20 border-b pb-2 font-semibold text-3xl text-primary tracking-tight first:mt-0'>
             Din profil
           </h2>
           <UpdateProfileForm userId={userId} />
         </div>
-        <div>
-          <h2 className='scroll-m-20 border-b pb-2 font-semibold text-3xl tracking-tight first:mt-0'>
-            Prikker
+        <div className='row-span-2'>
+          <h2 className='scroll-m-20 border-b pb-2 font-semibold text-3xl text-primary tracking-tight first:mt-0'>
+            Dine prikker
           </h2>
-          <UpdateProfileForm userId={userId} />
+          <Points userId={userId} />
         </div>
         <div>
-          <h2 className='scroll-m-20 border-b pb-2 font-semibold text-3xl tracking-tight first:mt-0'>
+          <h2 className='scroll-m-20 border-b pb-2 font-semibold text-3xl text-primary tracking-tight first:mt-0'>
             Dine kommende bedriftspresentasjoner
           </h2>
           <Registrations userId={userId} />
