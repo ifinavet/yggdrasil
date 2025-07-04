88d38040ea7e755ff58aed87caa5d4ed25eae1c4
Christoffer Hennie <christoffer@hennie.dev>
2025-07-04 13:23:06 +0200

    Add pages for companies, contact, and students with layout
    Wrap event admin page in ResponsiveCenterContainer for consistency

--- apps/midgard/src/app/events/[slug]/admin/page.tsx
+++ apps/midgard/src/app/events/[slug]/admin/page.tsx
@@ -1,11 +1,13 @@
+import ResponsiveCenterContainer from "@/components/common/responsive-center-container";
+
 export default async function AdminPage({ params }: { params: Promise<{ slug: number }> }) {
   const event_id = await params.then(({ slug }) => slug);
 
 
   return (
-    <div>
+    <ResponsiveCenterContainer>
       <h1>Hei!</h1>
       <p>{event_id}</p>
-    </div>
+    </ResponsiveCenterContainer>
   );
 }
