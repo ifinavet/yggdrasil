542b3225013070b90943c3fb05a20e77bb229398
Christoffer Hennie <christoffer@hennie.dev>
2025-07-11 11:14:43 +0200

    Add metadata titles to Midgard pages and update metadataBase

--- apps/midgard/src/app/profile/page.tsx
+++ apps/midgard/src/app/profile/page.tsx
@@ -7,45 +7,49 @@ import Registrations from "@/components/profile/registrations";
 import UpdateProfileForm from "@/components/profile/update-profile-form";
 import { getStudentById } from "@/lib/query/profile";
 
+export const metadata = {
+  title: "Profil",
+};
+
 export default async function ProfilePage() {
-	const { userId, redirectToSignIn } = await auth();
-	const user = await currentUser();
+  const { userId, redirectToSignIn } = await auth();
+  const user = await currentUser();
 
-	if (!userId || !user) {
-		return redirectToSignIn();
-	}
+  if (!userId || !user) {
+    return redirectToSignIn();
+  }
 
-	const queryClient = new QueryClient();
-	await queryClient.prefetchQuery({
-		queryKey: ["student", userId],
-		queryFn: () => getStudentById(userId),
-	});
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["student", userId],
+    queryFn: () => getStudentById(userId),
+  });
 
-	return (
-		<HydrationBoundary state={dehydrate(queryClient)}>
-			<ResponsiveCenterContainer>
-				<Title>Din Profil</Title>
-				<div className='grid max-w-full gap-6 align-top lg:grid-cols-2'>
-					<div>
-						<h2 className='scroll-m-20 border-b pb-2 font-semibold text-3xl text-primary tracking-tight first:mt-0'>
-							Din profil
-						</h2>
-						<UpdateProfileForm userId={userId} className='mt-4' />
-					</div>
-					<div className='row-span-2'>
-						<h2 className='scroll-m-20 border-b pb-2 font-semibold text-3xl text-primary tracking-tight first:mt-0'>
-							Dine prikker
-						</h2>
-						<Points userId={userId} className='mt-4' />
-					</div>
-					<div>
-						<h2 className='scroll-m-20 border-b pb-2 font-semibold text-3xl text-primary tracking-tight first:mt-0'>
-							Dine kommende bedriftspresentasjoner
-						</h2>
-						<Registrations userId={userId} className='mt-4' />
-					</div>
-				</div>
-			</ResponsiveCenterContainer>
-		</HydrationBoundary>
-	);
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ResponsiveCenterContainer>
+        <Title>Din Profil</Title>
+        <div className='grid max-w-full gap-6 align-top lg:grid-cols-2'>
+          <div>
+            <h2 className='scroll-m-20 border-b pb-2 font-semibold text-3xl text-primary tracking-tight first:mt-0'>
+              Din profil
+            </h2>
+            <UpdateProfileForm userId={userId} className='mt-4' />
+          </div>
+          <div className='row-span-2'>
+            <h2 className='scroll-m-20 border-b pb-2 font-semibold text-3xl text-primary tracking-tight first:mt-0'>
+              Dine prikker
+            </h2>
+            <Points userId={userId} className='mt-4' />
+          </div>
+          <div>
+            <h2 className='scroll-m-20 border-b pb-2 font-semibold text-3xl text-primary tracking-tight first:mt-0'>
+              Dine kommende bedriftspresentasjoner
+            </h2>
+            <Registrations userId={userId} className='mt-4' />
+          </div>
+        </div>
+      </ResponsiveCenterContainer>
+    </HydrationBoundary>
+  );
 }
