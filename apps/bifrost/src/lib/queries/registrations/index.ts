94b517479ab63c16b4037590839189911a5d5f8f
Christoffer Hennie <christoffer.hennie@outlook.com>
2025-07-25 12:55:18 +0200

    Remove Supabase and related queries from Bifrost app

--- apps/bifrost/src/lib/queries/registrations/index.ts
+++
@@ -1,5 +0,0 @@
-import deleteRegistration from "./delete";
-import getAllRegistrations from "./getAll";
-import updateRegistration from "./update";
-
-export { getAllRegistrations, deleteRegistration, updateRegistration };
--- apps/bifrost/src/lib/queries/resources/index.ts
+++
@@ -1,6 +0,0 @@
-import createResource from "./create";
-import getAllResources from "./getAll";
-import getResourceById from "./getById";
-import updateResource from "./update";
-
-export { createResource, getAllResources, getResourceById, updateResource };
