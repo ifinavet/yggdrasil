f76af428d8d1a0c19172d703ac28bf584b7865d4
Christoffer Hennie <christoffer.hennie@outlook.com>
2025-07-30 19:51:12 +0200

    Fix optional email validation in board member schema

--- apps/bifrost/src/constants/schemas/boardmember-form-schema.ts
+++ apps/bifrost/src/constants/schemas/boardmember-form-schema.ts
@@ -4,7 +4,7 @@ export const formSchema = z.object({
   userID: z.string(),
   role: z.string().min(2).max(100),
   group: z.string().min(2).max(100),
-  positionEmail: z.email().optional(),
+  positionEmail: z.optional(z.email()),
 });
 
 export type boardMemberSchema = z.infer<typeof formSchema>;
