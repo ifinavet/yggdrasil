10aa165d0b4b02df45927fc5a99a11917bc7d141
Christoffer Hennie <christoffer.hennie@outlook.com>
2025-08-01 13:24:06 +0200

    Update to use NEXT_PUBLIC_CONVEX_HOSTNAME env variable

--- apps/midgard/next.config.ts
+++ apps/midgard/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
-if (!process.env.CONVEX_HOSTNAME) {
-  throw new Error("CONVEX_HOSTNAME environment variable is not set.");
+if (!process.env.NEXT_PUBLIC_CONVEX_HOSTNAME) {
+  throw new Error("NEXT_PUBLIC_CONVEX_HOSTNAME environment variable is not set.");
 }
 
 const nextConfig: NextConfig = {
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: process.env.CONVEX_HOSTNAME,
+        hostname: process.env.NEXT_PUBLIC_CONVEX_HOSTNAME,
         port: "",
         pathname: "**",
       },
