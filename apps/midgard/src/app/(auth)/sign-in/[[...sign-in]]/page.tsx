573d053ce0673dea72ab8035c2959c05471af23e
Christoffer Hennie <christoffer@hennie.dev>
2025-08-10 22:23:55 +0200

    Redirect authenticated users from sign-in and sign-up pages
    Add admin layout with org authorization for event admin pages

--- apps/midgard/src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ apps/midgard/src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,12 +1,17 @@
-"use client"
-
 import ResponsiveCenterContainer from "@/components/common/responsive-center-container";
 import { SignIn } from "@clerk/nextjs";
-import { useSearchParams } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function SignInPage() {
-  const params = useSearchParams()
-  const redirectUrl = params.get("redirect") || "/";
+export default async function SignInPage() {
+  const { isAuthenticated } = await auth();
+  const headerList = await headers();
+  const searchParam = headerList.get("x-searchParams") || "/";
+
+  const redirectUrl = searchParam.includes("=") ? decodeURIComponent(searchParam.split("=")[1] || "/") : "/";
+
+  if (isAuthenticated) return redirect("/")
 
   return (
     <ResponsiveCenterContainer>
---
+++ apps/midgard/src/app/events/[slug]/admin/layout.tsx
@@ -0,0 +1,8 @@
+import { auth } from "@clerk/nextjs/server"
+
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  const { orgId } = await auth();
+  if (orgId !== process.env.NAVET_ORG_ID) throw new Error("Unauthorized access");
+
+  return children
+}
