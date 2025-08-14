4ff49a437829bd6a106d0516cfdc0b50640e6343
Christoffer Hennie <christoffer@hennie.dev>
2025-08-14 15:08:21 +0200

    Update middleware matcher to run Clerk on all routes except _next
    Resolves #72

--- apps/bifrost/src/middleware.ts
+++ apps/bifrost/src/middleware.ts
@@ -2,35 +2,35 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { type NextRequest, NextResponse } from "next/server";
 
 const searchParamsMiddleware = (request: NextRequest) => {
-	return NextResponse.next({
-		headers: {
-			"x-searchParams": request.nextUrl.searchParams.toString(),
-			"x-pathname": request.nextUrl.pathname,
-		},
-	});
+  return NextResponse.next({
+    headers: {
+      "x-searchParams": request.nextUrl.searchParams.toString(),
+      "x-pathname": request.nextUrl.pathname,
+    },
+  });
 };
 
 const publicRoute = createRouteMatcher(["/", "/api(.*)"]);
 
 export const middleware = clerkMiddleware(async (auth, req) => {
-	const { orgId } = await auth();
+  const { orgId } = await auth();
 
-	if (!publicRoute(req)) {
-		await auth.protect();
+  if (!publicRoute(req)) {
+    await auth.protect();
 
-		if (orgId !== process.env.NAVET_ORG_ID) {
-			return NextResponse.redirect(new URL("/", req.url));
-		}
-	}
+    if (orgId !== process.env.NAVET_ORG_ID) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  }
 
-	return searchParamsMiddleware(req);
+  return searchParamsMiddleware(req);
 });
 
 export const config = {
-	matcher: [
-		// Skip Next.js internals and all static files, unless found in search params
-		"/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-		// Always run for API routes
-		"/(api|trpc)(.*)",
-	],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };
