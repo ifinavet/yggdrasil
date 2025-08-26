ecd18b76429fdbafebd9e8b6ef1e013f049a5f65
Christoffer Hennie <christoffer@hennie.dev>
2025-08-26 20:33:40 +0200

    Add main sponsor management to organization page

---
+++ apps/bifrost/src/components/organization/main-sponsor/update-main-sponsor.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import type { api } from "@workspace/backend/convex/api";
+import { Button } from "@workspace/ui/components/button";
+import {
+	Card,
+	CardAction,
+	CardContent,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@workspace/ui/components/card";
+import { type Preloaded, usePreloadedQuery } from "convex/react";
+import { Pencil } from "lucide-react";
+import Link from "next/link";
+import SafeHtml from "@/components/common/sanitize-html";
+import UpdateMainSponsorForm from "./update-main-sponsor-form";
+
+export default function UpdateMainSponsor({
+	preloadedMainSponsor,
+}: {
+	preloadedMainSponsor: Preloaded<typeof api.companies.getMainSponsor>;
+}) {
+	const mainSponsor = usePreloadedQuery(preloadedMainSponsor);
+
+	return mainSponsor && (
+		<div className="flex flex-wrap gap-4">
+			<UpdateMainSponsorForm companyId={mainSponsor._id} />
+			<Link href={`/companies/${mainSponsor._id}`} className="max-w-lg">
+				<Card>
+					<CardHeader>
+						<CardTitle>{mainSponsor.name}</CardTitle>
+						<CardAction>
+							<Button variant='outline' size='icon'>
+								<Pencil />
+							</Button>
+						</CardAction>
+					</CardHeader>
+					<CardContent>
+						{mainSponsor.description && (
+							<SafeHtml
+								html={mainSponsor.description}
+								className='prose dark:prose-invert h-32 overflow-hidden overflow-ellipsis *:text-ellipsis'
+							/>
+						)}
+					</CardContent>
+					<CardFooter>
+						<p>Org. nr: {mainSponsor.orgNumber ?? "N/A"}</p>
+					</CardFooter>
+				</Card>
+			</Link>
+		</div>
+	);
+}
