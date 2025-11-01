29911c31e5409a8f9cf1f0a02da9c5c185aa370e
christofferhennie <christoffer@hennie.dev>
2025-11-01 21:37:17 +0100

    Add color for "Ikke registrert" status

--- apps/bifrost/src/components/students/students-overview.tsx
+++ apps/bifrost/src/components/students/students-overview.tsx
@@ -18,7 +18,8 @@ type StudentColumns = Doc<"students"> & {
 const statusColors = {
 	Ukjent: "bg-yellow-100 text-yellow-800",
 	Aktiv: "bg-green-100 text-green-800",
-};
+	"Ikke registrert": "bg-red-100 text-red-800",
+} as const;
 
 const createColumns: ColumnDef<StudentColumns>[] = [
 	{
