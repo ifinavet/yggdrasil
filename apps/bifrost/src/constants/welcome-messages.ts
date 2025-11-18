5598ed9f38f47b197ce8a4b500e55f43d545847b
danielbrunvoll <danielnilsenbrunvoll@gmail.com>
2025-11-18 13:29:57 +0100

    Added vedtekter to footer and fixed typos in welcome-messages

--- apps/bifrost/src/constants/welcome-messages.ts
+++ apps/bifrost/src/constants/welcome-messages.ts
@@ -1,9 +1,9 @@
 export const WELCOME_MESSAGES = [
 	"God søndag! Håper du har hatt en flott uke!",
 	"Er det Mandag igjen? Så bra! Nydelig mulighet til å få gjort litt arbeid",
-	"Sa noen tisdag? Bedpress tid!",
-	"God onsdag! Vi er allerede halvies!",
-	"Torsdag?! Det er jo også en bedpress dag!",
-	"Fredag! Engelig helg! Skal du noe gøy?",
+	"Sa noen tirsdag? Bedpres tid!",
+	"God onsdag! Vi er allerede halvveis!",
+	"Torsdag?! Det er jo også en bedpres dag!",
+	"Fredag! Endelig helg! Skal du noe gøy?",
 	"Det er jo lørdag! Litt jobbing, også kan vi gjøre noe gøy, kanskje en tur på byen i kveld?",
 ];
