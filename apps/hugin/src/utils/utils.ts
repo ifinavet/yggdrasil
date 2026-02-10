8fddd3a9ce1e1bb50fe55e69c08655f5c2eeadcf
Christoffer H <developer@hennie.dev>
2026-02-10 23:37:49 +0100

    Removed leftovers from duplicating bifrost to make hugin

--- apps/hugin/src/utils/utils.ts
+++
@@ -1,9 +0,0 @@
-export function humanReadableDate(date: Date): string {
-	return date.toLocaleString("no-NO", {
-		year: "numeric",
-		month: "long",
-		day: "2-digit",
-		hour: "2-digit",
-		minute: "2-digit",
-	});
-}
