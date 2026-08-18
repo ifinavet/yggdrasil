59837a03d33449e5481a3d09e252f1597aa7a13e
vuhger <victor.uhnger@gmail.com>
2026-08-18 20:17:29 +0200

    chore: remove dead exports from link-popover

--- apps/bifrost/src/components/common/forms/markdown-editor/link-popover.tsx
+++ apps/bifrost/src/components/common/forms/markdown-editor/link-popover.tsx
@@ -23,7 +23,7 @@ export interface LinkHandlerProps {
 	readonly onLinkActive?: () => void;
 }
 
-export interface LinkMainProps {
+interface LinkMainProps {
 	readonly url: string;
 	readonly setUrl: React.Dispatch<React.SetStateAction<string>>;
 	readonly setLink: () => void;
@@ -31,7 +31,7 @@ export interface LinkMainProps {
 	readonly isActive: boolean;
 }
 
-export const useLinkHandler = (props: Readonly<LinkHandlerProps>) => {
+const useLinkHandler = (props: Readonly<LinkHandlerProps>) => {
 	const { editor, onSetLink, onLinkActive } = props;
 	const [url, setUrl] = React.useState<string>("");
 
@@ -103,20 +103,6 @@ export const useLinkHandler = (props: Readonly<LinkHandlerProps>) => {
 		removeLink,
 		isActive: editor?.isActive("link") || false,
 	};
-};
-
-export const LinkContent: React.FC<
-	Readonly<{
-		editor?: Editor | null;
-	}>
-> = ({ editor: providedEditor }) => {
-	const editor = useTiptapEditor(providedEditor);
-
-	const linkHandler = useLinkHandler({
-		editor: editor,
-	});
-
-	return <LinkMain {...linkHandler} />;
 };
 
 const LinkMain: React.FC<Readonly<LinkMainProps>> = ({
