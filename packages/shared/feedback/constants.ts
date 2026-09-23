2845e090d3f058852bcf642dd07c8dac63053d3a
vuhger <victor.uhnger@gmail.com>
2026-09-23 23:16:00 +0200

    refactor(feedback): centralize rollout flags and preview configuration

---
+++ packages/shared/feedback/constants.ts
@@ -0,0 +1,9 @@
+// Change these values in code when rolling out feedback. Public approved report links do not use these switches.
+export const feedbackConfig = {
+	previewStorageKey: "hugin-feedback-preview",
+	uiEnabled: false,
+	emailsEnabled: false,
+	reportsEnabled: false,
+	reportEmailsEnabled: false,
+	huginBaseUrl: "https://hugin.ifinavet.no",
+};
